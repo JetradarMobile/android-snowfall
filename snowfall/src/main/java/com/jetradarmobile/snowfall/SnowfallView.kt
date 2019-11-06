3d70415cb0b3bb232c49759027cd4fdc4ec805aa
Rojan Thomas <rojan.thomas@whitbread.com>
2019-11-06 13:22:27 +0000

    Adding multi-bitmap support, various gradle/kotlin upgrades

--- snowfall/src/main/java/com/jetradarmobile/snowfall/SnowfallView.kt
+++ snowfall/src/main/java/com/jetradarmobile/snowfall/SnowfallView.kt
@@ -16,143 +16,200 @@
 
 package com.jetradarmobile.snowfall
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.AttributeSet
+import android.util.LruCache
 import android.view.View
+import androidx.core.content.res.use
 
-class SnowfallView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-  private val DEFAULT_SNOWFLAKES_NUM = 200
-  private val DEFAULT_SNOWFLAKE_ALPHA_MIN = 150
-  private val DEFAULT_SNOWFLAKE_ALPHA_MAX = 250
-  private val DEFAULT_SNOWFLAKE_ANGLE_MAX = 10
-  private val DEFAULT_SNOWFLAKE_SIZE_MIN_IN_DP = 2
-  private val DEFAULT_SNOWFLAKE_SIZE_MAX_IN_DP = 8
-  private val DEFAULT_SNOWFLAKE_SPEED_MIN = 2
-  private val DEFAULT_SNOWFLAKE_SPEED_MAX = 8
-  private val DEFAULT_SNOWFLAKES_FADING_ENABLED = false
-  private val DEFAULT_SNOWFLAKES_ALREADY_FALLING = false
+internal class SnowfallView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-  private val snowflakesNum: Int
-  private val snowflakeImage: Bitmap?
-  private val snowflakeAlphaMin: Int
-  private val snowflakeAlphaMax: Int
-  private val snowflakeAngleMax: Int
-  private val snowflakeSizeMinInPx: Int
-  private val snowflakeSizeMaxInPx: Int
-  private val snowflakeSpeedMin: Int
-  private val snowflakeSpeedMax: Int
-  private val snowflakesFadingEnabled: Boolean
-  private val snowflakesAlreadyFalling: Boolean
+    private val snowflakesNum: Int
+    private val snowflakeImage: Bitmap?
+    private var snowflakeImages: LruCache<Int, Bitmap>?
+    private val snowflakeAlphaMin: Int
+    private val snowflakeAlphaMax: Int
+    private val snowflakeAngleMax: Int
+    private val snowflakeSizeMinInPx: Int
+    private val snowflakeSizeMaxInPx: Int
+    private val snowflakeSpeedMin: Int
+    private val snowflakeSpeedMax: Int
+    private val snowflakesFadingEnabled: Boolean
+    private val snowflakesAlreadyFalling: Boolean
 
-  private lateinit var updateSnowflakesThread: UpdateSnowflakesThread
-  private var snowflakes: Array<Snowflake>? = null
-
-  init {
-    val a = context.obtainStyledAttributes(attrs, R.styleable.SnowfallView)
-    try {
-      snowflakesNum = a.getInt(R.styleable.SnowfallView_snowflakesNum, DEFAULT_SNOWFLAKES_NUM)
-      snowflakeImage = a.getDrawable(R.styleable.SnowfallView_snowflakeImage)?.toBitmap()
-      snowflakeAlphaMin = a.getInt(R.styleable.SnowfallView_snowflakeAlphaMin, DEFAULT_SNOWFLAKE_ALPHA_MIN)
-      snowflakeAlphaMax = a.getInt(R.styleable.SnowfallView_snowflakeAlphaMax, DEFAULT_SNOWFLAKE_ALPHA_MAX)
-      snowflakeAngleMax = a.getInt(R.styleable.SnowfallView_snowflakeAngleMax, DEFAULT_SNOWFLAKE_ANGLE_MAX)
-      snowflakeSizeMinInPx = a.getDimensionPixelSize(R.styleable.SnowfallView_snowflakeSizeMin, dpToPx(DEFAULT_SNOWFLAKE_SIZE_MIN_IN_DP))
-      snowflakeSizeMaxInPx = a.getDimensionPixelSize(R.styleable.SnowfallView_snowflakeSizeMax, dpToPx(DEFAULT_SNOWFLAKE_SIZE_MAX_IN_DP))
-      snowflakeSpeedMin = a.getInt(R.styleable.SnowfallView_snowflakeSpeedMin, DEFAULT_SNOWFLAKE_SPEED_MIN)
-      snowflakeSpeedMax = a.getInt(R.styleable.SnowfallView_snowflakeSpeedMax, DEFAULT_SNOWFLAKE_SPEED_MAX)
-      snowflakesFadingEnabled = a.getBoolean(R.styleable.SnowfallView_snowflakesFadingEnabled, DEFAULT_SNOWFLAKES_FADING_ENABLED)
-      snowflakesAlreadyFalling = a.getBoolean(R.styleable.SnowfallView_snowflakesAlreadyFalling, DEFAULT_SNOWFLAKES_ALREADY_FALLING)
-    } finally {
-      a.recycle()
-    }
-
-  }
-
-  override fun onAttachedToWindow() {
-    super.onAttachedToWindow()
-    updateSnowflakesThread = UpdateSnowflakesThread()
-  }
-
-  override fun onDetachedFromWindow() {
-    updateSnowflakesThread.quit()
-    super.onDetachedFromWindow()
-  }
-
-  private fun dpToPx(dp: Int): Int {
-    return (dp * resources.displayMetrics.density).toInt()
-  }
-
-  override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-    super.onSizeChanged(w, h, oldw, oldh)
-    snowflakes = createSnowflakes()
-  }
-
-  override fun onVisibilityChanged(changedView: View, visibility: Int) {
-    super.onVisibilityChanged(changedView, visibility)
-    if (changedView === this && visibility == GONE) {
-      snowflakes?.forEach { it.reset() }
-    }
-  }
-
-  override fun onDraw(canvas: Canvas) {
-    super.onDraw(canvas)
-    if (isInEditMode) {
-      return
-    }
-    val fallingSnowflakes = snowflakes?.filter { it.isStillFalling() }
-    if (fallingSnowflakes?.isNotEmpty() == true) {
-      fallingSnowflakes.forEach { it.draw(canvas) }
-      updateSnowflakes()
-    }
-    else {
-      visibility = GONE
-    }
-  }
-
-  fun stopFalling() {
-    snowflakes?.forEach { it.shouldRecycleFalling = false }
-  }
-
-  fun restartFalling() {
-    snowflakes?.forEach { it.shouldRecycleFalling = true }
-    visibility = VISIBLE
-  }
-
-  private fun createSnowflakes(): Array<Snowflake> {
-    val snowflakeParams = Snowflake.Params(
-        parentWidth = width,
-        parentHeight = height,
-        image = snowflakeImage,
-        alphaMin = snowflakeAlphaMin,
-        alphaMax = snowflakeAlphaMax,
-        angleMax = snowflakeAngleMax,
-        sizeMinInPx = snowflakeSizeMinInPx,
-        sizeMaxInPx = snowflakeSizeMaxInPx,
-        speedMin = snowflakeSpeedMin,
-        speedMax = snowflakeSpeedMax,
-        fadingEnabled = snowflakesFadingEnabled,
-        alreadyFalling = snowflakesAlreadyFalling)
-    return Array(snowflakesNum) { Snowflake(snowflakeParams) }
-  }
-
-  private fun updateSnowflakes() {
-    val fallingSnowflakes = snowflakes?.filter { it.isStillFalling() }
-    if (fallingSnowflakes?.isNotEmpty() == true) {
-      updateSnowflakesThread.handler.post {
-        fallingSnowflakes.forEach { it.update() }
-        postInvalidateOnAnimation()
-      }
-    }
-  }
-
-  private class UpdateSnowflakesThread : HandlerThread("SnowflakesComputations") {
-    val handler by lazy { Handler(looper) }
+    private lateinit var updateSnowflakesThread: UpdateSnowflakesThread
+    private var snowflakes: Array<Snowflake>? = null
 
     init {
-      start()
+        val a = context.obtainStyledAttributes(attrs, R.styleable.SnowfallView)
+        try {
+            snowflakesNum = a.getInt(R.styleable.SnowfallView_snowflakesNum, DEFAULT_SNOWFLAKES_NUM)
+            snowflakeAlphaMin = a.getInt(R.styleable.SnowfallView_snowflakeAlphaMin, DEFAULT_SNOWFLAKE_ALPHA_MIN)
+            snowflakeAlphaMax = a.getInt(R.styleable.SnowfallView_snowflakeAlphaMax, DEFAULT_SNOWFLAKE_ALPHA_MAX)
+            snowflakeAngleMax = a.getInt(R.styleable.SnowfallView_snowflakeAngleMax, DEFAULT_SNOWFLAKE_ANGLE_MAX)
+            snowflakeSizeMinInPx = a.getDimensionPixelSize(
+                    R.styleable.SnowfallView_snowflakeSizeMin,
+                    dpToPx(DEFAULT_SNOWFLAKE_SIZE_MIN_IN_DP)
+            )
+            snowflakeSizeMaxInPx = a.getDimensionPixelSize(
+                    R.styleable.SnowfallView_snowflakeSizeMax,
+                    dpToPx(DEFAULT_SNOWFLAKE_SIZE_MAX_IN_DP)
+            )
+            snowflakeSpeedMin = a.getInt(R.styleable.SnowfallView_snowflakeSpeedMin, DEFAULT_SNOWFLAKE_SPEED_MIN)
+            snowflakeSpeedMax = a.getInt(R.styleable.SnowfallView_snowflakeSpeedMax, DEFAULT_SNOWFLAKE_SPEED_MAX)
+            snowflakesFadingEnabled =
+                    a.getBoolean(R.styleable.SnowfallView_snowflakesFadingEnabled, DEFAULT_SNOWFLAKES_FADING_ENABLED)
+            snowflakesAlreadyFalling =
+                    a.getBoolean(R.styleable.SnowfallView_snowflakesAlreadyFalling, DEFAULT_SNOWFLAKES_ALREADY_FALLING)
+
+            snowflakeImage = a.getDrawable(R.styleable.SnowfallView_snowflakeImage)?.toBitmap()
+            snowflakeImages = run {
+                val drawableArray = a.getResourceId(R.styleable.SnowfallView_snowflakeImages, -1)
+                createBitmapCache(drawableArray)
+            }
+        } finally {
+            a.recycle()
+        }
     }
-  }
+
+    private fun createBitmapCache(drawableArray: Int): LruCache<Int, Bitmap>? {
+        if (drawableArray != -1) {
+            val typedArray = resources.obtainTypedArray(drawableArray)
+            val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+            val cacheSize = maxMemory / 8
+            val bitmapsCache = object : LruCache<Int, Bitmap>(cacheSize) {
+                override fun sizeOf(key: Int, value: Bitmap): Int {
+                    return value.byteCount / 1024
+                }
+
+                @SuppressLint("Recycle")
+                override fun create(key: Int): Bitmap? {
+                    resources.obtainTypedArray(drawableArray).use {
+                        return it.getDrawable(key)?.toBitmap()
+                    }
+                }
+            }
+            for (i in 0 until typedArray.length()) {
+                bitmapsCache.put(i, typedArray.getDrawable(i)?.toBitmap())
+            }
+            typedArray.recycle()
+            return bitmapsCache
+        } else {
+            return null
+        }
+    }
+
+    fun setSnowflakes(drawableArray: Int) {
+        snowflakeImages?.evictAll()
+        snowflakeImages = createBitmapCache(drawableArray)
+        snowflakes = createSnowflakes()
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        updateSnowflakesThread = UpdateSnowflakesThread()
+    }
+
+    override fun onDetachedFromWindow() {
+        updateSnowflakesThread.quit()
+        super.onDetachedFromWindow()
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        return (dp * resources.displayMetrics.density).toInt()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        snowflakes = createSnowflakes()
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (changedView === this && visibility == GONE) {
+            snowflakes?.forEach { it.reset() }
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (isInEditMode) {
+            return
+        }
+        val fallingSnowflakes = snowflakes?.filter { it.isStillFalling() }
+        if (fallingSnowflakes?.isNotEmpty() == true) {
+            fallingSnowflakes.forEach { it.draw(canvas) }
+            updateSnowflakes()
+        } else {
+            visibility = GONE
+        }
+    }
+
+    fun stopFalling() {
+        snowflakes?.forEach { it.shouldRecycleFalling = false }
+    }
+
+    fun restartFalling() {
+        snowflakes?.forEach { it.shouldRecycleFalling = true }
+        visibility = VISIBLE
+    }
+
+    private fun createSnowflakes(): Array<Snowflake> {
+        val snowflakeParams = Snowflake.Params(
+                parentWidth = width,
+                parentHeight = height,
+                image = snowflakeImage,
+                images = snowflakeImages,
+                alphaMin = snowflakeAlphaMin,
+                alphaMax = snowflakeAlphaMax,
+                angleMax = snowflakeAngleMax,
+                sizeMinInPx = snowflakeSizeMinInPx,
+                sizeMaxInPx = snowflakeSizeMaxInPx,
+                speedMin = snowflakeSpeedMin,
+                speedMax = snowflakeSpeedMax,
+                fadingEnabled = snowflakesFadingEnabled,
+                alreadyFalling = snowflakesAlreadyFalling
+        )
+        return Array(snowflakesNum) { Snowflake(snowflakeParams) }
+    }
+
+    private fun updateSnowflakes() {
+        val fallingSnowflakes = snowflakes?.filter { it.isStillFalling() }
+        if (isStillFalling()) {
+            updateSnowflakesThread.handler.post {
+                fallingSnowflakes?.forEach { it.update() }
+                postInvalidateOnAnimation()
+            }
+        }
+    }
+
+    //Accessed via xml file
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun isStillFalling(): Boolean = !snowflakes?.filter { it.isStillFalling() }.isNullOrEmpty()
+
+    private class UpdateSnowflakesThread : HandlerThread("SnowflakesComputations") {
+        val handler by lazy { Handler(looper) }
+
+        init {
+            start()
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_SNOWFLAKES_NUM = 200
+        private const val DEFAULT_SNOWFLAKE_ALPHA_MIN = 150
+        private const val DEFAULT_SNOWFLAKE_ALPHA_MAX = 250
+        private const val DEFAULT_SNOWFLAKE_ANGLE_MAX = 10
+        private const val DEFAULT_SNOWFLAKE_SIZE_MIN_IN_DP = 2
+        private const val DEFAULT_SNOWFLAKE_SIZE_MAX_IN_DP = 8
+        private const val DEFAULT_SNOWFLAKE_SPEED_MIN = 2
+        private const val DEFAULT_SNOWFLAKE_SPEED_MAX = 8
+        private const val DEFAULT_SNOWFLAKES_FADING_ENABLED = false
+        private const val DEFAULT_SNOWFLAKES_ALREADY_FALLING = false
+    }
 }
--- snowfall/src/main/java/com/jetradarmobile/snowfall/Snowflake.kt
+++ snowfall/src/main/java/com/jetradarmobile/snowfall/Snowflake.kt
@@ -19,107 +19,126 @@ package com.jetradarmobile.snowfall
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.Paint.Style
+import android.util.LruCache
 import java.lang.Math.cos
 import java.lang.Math.sin
 import java.lang.Math.toRadians
 
 internal class Snowflake(val params: Params) {
-  private var size: Int = 0
-  private var alpha: Int = 255
-  private var bitmap: Bitmap? = null
-  private var speedX: Double = 0.0
-  private var speedY: Double = 0.0
-  private var positionX: Double = 0.0
-  private var positionY: Double = 0.0
 
-  private val paint by lazy {
-    Paint(Paint.ANTI_ALIAS_FLAG).apply {
-      color = Color.rgb(255, 255, 255)
-      style = Style.FILL
-    }
-  }
-  private val randomizer by lazy { Randomizer() }
+    private var size: Int = 0
+    private var alpha: Int = 255
+    private lateinit var bitmap: Bitmap
+    private var speedX: Double = 0.0
+    private var speedY: Double = 0.0
+    private var positionX: Double = 0.0
+    private var positionY: Double = 0.0
 
-  var shouldRecycleFalling = true
-  private var stopped = false
+    private val matrix = Matrix()
 
-  init {
-    reset()
-  }
-
-  internal fun reset(positionY: Double? = null) {
-    shouldRecycleFalling = true
-    size = randomizer.randomInt(params.sizeMinInPx, params.sizeMaxInPx, gaussian = true)
-    if (params.image != null) {
-      bitmap = Bitmap.createScaledBitmap(params.image, size, size, false)
-    }
-
-    val speed = ((size - params.sizeMinInPx).toFloat() / (params.sizeMaxInPx - params.sizeMinInPx) *
-        (params.speedMax - params.speedMin) + params.speedMin)
-    val angle = toRadians(randomizer.randomDouble(params.angleMax) * randomizer.randomSignum())
-    speedX = speed * sin(angle)
-    speedY = speed * cos(angle)
-
-    alpha = randomizer.randomInt(params.alphaMin, params.alphaMax)
-    paint.alpha = alpha
-
-    positionX = randomizer.randomDouble(params.parentWidth)
-    if (positionY != null) {
-      this.positionY = positionY
-    } else {
-      this.positionY = randomizer.randomDouble(params.parentHeight)
-      if (!params.alreadyFalling) {
-        this.positionY = this.positionY - params.parentHeight - size
-      }
-    }
-  }
-
-  fun isStillFalling(): Boolean {
-    return (shouldRecycleFalling || (positionY > 0 && positionY < params.parentHeight))
-  }
-
-  fun update() {
-    positionX += speedX
-    positionY += speedY
-    if (positionY > params.parentHeight) {
-      if (shouldRecycleFalling) {
-        if (stopped) {
-          stopped = false
-          reset()
-        } else {
-          reset(positionY = -size.toDouble())
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.WHITE
+            style = Style.FILL
         }
-      } else {
-        positionY = params.parentHeight + size.toDouble()
-        stopped = true
-      }
     }
-    if (params.fadingEnabled) {
-      paint.alpha = (alpha * ((params.parentHeight - positionY).toFloat() / params.parentHeight)).toInt()
-    }
-  }
+    private val randomizer by lazy { Randomizer() }
 
-  fun draw(canvas: Canvas) {
-    if (bitmap != null) {
-      canvas.drawBitmap(bitmap, positionX.toFloat(), positionY.toFloat(), paint)
-    } else {
-      canvas.drawCircle(positionX.toFloat(), positionY.toFloat(), size.toFloat(), paint)
-    }
-  }
+    var shouldRecycleFalling = true
+    private var stopped = false
 
-  data class Params(
-      val parentWidth: Int,
-      val parentHeight: Int,
-      val image: Bitmap?,
-      val alphaMin: Int,
-      val alphaMax: Int,
-      val angleMax: Int,
-      val sizeMinInPx: Int,
-      val sizeMaxInPx: Int,
-      val speedMin: Int,
-      val speedMax: Int,
-      val fadingEnabled: Boolean,
-      val alreadyFalling: Boolean)
+    init {
+        reset()
+    }
+
+    internal fun reset(positionY: Double? = null) {
+        shouldRecycleFalling = true
+        size = randomizer.randomInt(params.sizeMinInPx, params.sizeMaxInPx, gaussian = true)
+        if (params.image != null) {
+            bitmap = Bitmap.createScaledBitmap(params.image, size, size, false)
+        } else if (params.images != null) {
+            bitmap = Bitmap.createScaledBitmap(
+                    params.images.get(randomizer.randomInt(0, params.images.putCount() - 1)),
+                    size,
+                    size,
+                    false
+            )
+        }
+
+        val speed = ((size - params.sizeMinInPx).toFloat() / (params.sizeMaxInPx - params.sizeMinInPx) *
+                (params.speedMax - params.speedMin) + params.speedMin)
+        val angle = toRadians(randomizer.randomDouble(params.angleMax) * randomizer.randomSignum())
+        speedX = speed * sin(angle)
+        speedY = speed * cos(angle)
+
+        alpha = randomizer.randomInt(params.alphaMin, params.alphaMax)
+        paint.alpha = alpha
+
+        positionX = randomizer.randomDouble(params.parentWidth)
+        if (positionY != null) {
+            this.positionY = positionY
+        } else {
+            this.positionY = randomizer.randomDouble(params.parentHeight)
+            if (!params.alreadyFalling) {
+                this.positionY = this.positionY - params.parentHeight - size
+            }
+        }
+    }
+
+    fun isStillFalling(): Boolean {
+        return (shouldRecycleFalling || (positionY > 0 && positionY < params.parentHeight))
+    }
+
+    fun update() {
+        positionX += speedX
+        positionY += speedY
+        if (positionY > params.parentHeight) {
+            if (shouldRecycleFalling) {
+                if (stopped) {
+                    stopped = false
+                    reset()
+                } else {
+                    reset(positionY = -size.toDouble())
+                }
+            } else {
+                positionY = params.parentHeight + size.toDouble()
+                stopped = true
+            }
+        }
+        if (params.fadingEnabled) {
+            paint.alpha = (alpha * ((params.parentHeight - positionY).toFloat() / params.parentHeight)).toInt()
+        }
+    }
+
+    fun draw(canvas: Canvas) {
+        matrix.postRotate(ROTATION * (params.parentHeight - positionY).toFloat() / params.parentHeight)
+        matrix.postTranslate(positionX.toFloat(), positionY.toFloat())
+
+        canvas.drawBitmap(bitmap, matrix, paint.takeIf { params.fadingEnabled })
+
+        matrix.reset()
+    }
+
+    data class Params(
+            val parentWidth: Int,
+            val parentHeight: Int,
+            val image: Bitmap?,
+            val images: LruCache<Int, Bitmap>?,
+            val alphaMin: Int,
+            val alphaMax: Int,
+            val angleMax: Int,
+            val sizeMinInPx: Int,
+            val sizeMaxInPx: Int,
+            val speedMin: Int,
+            val speedMax: Int,
+            val fadingEnabled: Boolean,
+            val alreadyFalling: Boolean
+    )
+
+    companion object {
+        private const val ROTATION = 360
+    }
 }
