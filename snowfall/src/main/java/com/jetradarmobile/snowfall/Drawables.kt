29af01df24d826cdde07bc87c286b62b4eceb410
Ayça Şenler <asenler@MagiClick.com>
2021-06-28 14:59:50 +0300

    no message

--- snowfall/src/main/java/com/jetradarmobile/snowfall/Drawables.kt
+++ snowfall/src/main/java/com/jetradarmobile/snowfall/Drawables.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//test
 package com.jetradarmobile.snowfall
 
 import android.annotation.SuppressLint
